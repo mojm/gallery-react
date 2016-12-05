505c10fce64bba4015101980b68db4cbefb4260c
mojm <mjm-mars@163.com>
2016-12-06 00:23:53 +0800

    init program structure

---
+++ src/components/Main.js
@@ -0,0 +1,58 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+
+import React from 'react';
+
+//获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+    var singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageDatas);
+
+var ImgFigure = React.createClass({
+  render: function() {
+    return (
+      <figure>
+        <img src={this.props.data.imageURL} alt={this.props.data.title} />
+        <figcaption>
+          <h2>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+});
+
+var AppComponent = React.createClass({
+  render: function() {
+
+    var controllerUnits = [],
+      imgFigures = [];
+
+    imageDatas.forEach(function(value) {
+      imgFigures.push(<ImgFigure data={value}/>);
+    });
+    return (
+      <section className="stage">
+        <section className="img-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+          {controllerUnits}
+        </nav>
+      </section>
+    );
+  }
+});
+
+AppComponent.defaultProps = {};
+
+export default AppComponent;
